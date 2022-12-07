dae98404b317bee38a9820c9b80715138b36312d
mohamadn607 <mohamad.n@tatvasoft.com>
2022-12-07 16:43:17 +0530

    updated

--- main-test.py
+++ main-test.py
@@ -2,9 +2,9 @@
 from main import Multiply
 
 def TestMultiply():
-        assert Multiply(2,3) == 6
-        assert Multiply(5,5) == 25
-        print("Add Function works correctly")
+        assert Multiply(2,4) == 8
+        assert Multiply(5,6) == 30
+        print("Multiply Function works correctly")
 
 if __name__ == '__main__':
-        TestAdd()
+        TestMultiply()
